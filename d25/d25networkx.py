b63de9fb5410bdf0c7a08a4af6352449ccd21e25
dante7867 <dante7867@gmail.com>
2023-12-25 14:23:44 +0100

    Minor refactoring for d25

--- d25/d25networkx.py
+++ d25/d25networkx.py
@@ -14,11 +14,11 @@ with open('in.txt', 'r') as f:
         key = key.strip('')
         vals = [v.strip() for v in vals[1:].split()]
         for nb in vals:
-            G.add_edge(key, nb,capacity=1.0)
+            G.add_edge(key, nb, capacity=1.0)
             
 nodes = list(G.nodes)
 for n1, n2 in zip(nodes, nodes[1:]):
-    cut_value, partition = nx.minimum_cut(G, n1, n2)
+    _, partition = nx.minimum_cut(G, n1, n2)
     reachable, non_reachable = partition
     if len(reachable) > 1 and len(non_reachable) > 1:
         print('p2:', len(reachable)*len(non_reachable)) 
